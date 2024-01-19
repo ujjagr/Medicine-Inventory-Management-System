e60b2fab53613718168ae7db60de76196dda9e43
Ujjawal Agrawal <ujjawal13agrawal@gmail.com>
2024-01-19 15:56:59 +0530

    Medicine Inventory Management System in C

---
+++ medicine_inventory.cpp
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <string.h>
+
+// Structure for storing information of medicine
+typedef struct {
+    char ID[50];
+    char nameMed[50];
+    char nameMan[50];
+    char manDate[20];
+    char expDate[20];
+    int quantity;
+    float price;
+    char type[20];
+} medicine;
+
+// Function prototypes
+void addMedicine(medicine arr[], int *medCount);
+void purchaseMedicine(medicine arr[], int medCount);
+void removeMedicine(medicine arr[], int *medCount);
+void displayMedicineStatistics(medicine arr[], int medCount);
+void printBill(medicine buy[], int buyCount);
+
+int main() {
+    medicine arr[50];
+    int choice, medCount = 0;
+
+    do {
+        // Printing the choices
+        printf("\nCHOICES:\n");
+        printf("1. Add Medicine\n");
+        printf("2. Purchase Medicine\n");
+        printf("3. Remove Medicine\n");
+        printf("4. Medicine Statistics\n");
+        printf("5. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addMedicine(arr, &medCount);
+                break;
+            case 2:
+                purchaseMedicine(arr, medCount);
+                break;
+            case 3:
+                removeMedicine(arr, &medCount);
+                break;
+            case 4:
+                displayMedicineStatistics(arr, medCount);
+                break;
+            case 5:
+                printf("Exiting the program\n");
+                break;
+            default:
+                printf("Enter a valid choice!\n");
+                break;
+        }
+
+    } while (choice != 5);
+
+    return 0;
+}
+
+void addMedicine(medicine arr[], int *medCount) {
+    char opt;
+
+    do {
+        char tempID[50];
+        printf("Enter medicine ID: ");
+        scanf("%s", tempID);
+
+        int i;
+        for (i = 0; i < *medCount; i++) {
+            if (strcmp(tempID, arr[i].ID) == 0) {
+                int addQuant;
+                printf("Enter the quantity: ");
+                scanf("%d", &addQuant);
+
+                arr[i].quantity += addQuant;
+
+                break;
+            }
+        }
+
+        if (i == *medCount) {
+            strcpy(arr[*medCount].ID, tempID);
+
+            printf("Enter medicine name: ");
+            scanf("%s", arr[*medCount].nameMed);
+
+            printf("Enter manufacturer name: ");
+            scanf("%s", arr[*medCount].nameMan);
+
+            printf("Enter manufacturing date: ");
+            scanf("%s", arr[*medCount].manDate);
+
+            printf("Enter expiry date: ");
+            scanf("%s", arr[*medCount].expDate);
+
+            printf("Enter quantity: ");
+            scanf("%d", &arr[*medCount].quantity);
+
+            printf("Enter price per quantity: ");
+            scanf("%f", &arr[*medCount].price);
+
+            printf("Enter type: ");
+            scanf("%s", arr[*medCount].type);
+
+            (*medCount)++;
+        }
+
+        printf("Do you wish to add more medicine?\nAns: ");
+        scanf(" %c", &opt);
+    } while (opt == 'y');
+}
+
+void purchaseMedicine(medicine arr[], int medCount) {
+    char opt;
+    int buyCount = 0;
+    medicine buy[50];
+
+    do {
+        char tempName[50], tempType[20];
+        int tempQuant;
+
+        printf("Enter medicine name: ");
+        scanf("%s", tempName);
+        printf("Enter medicine type: ");
+        scanf("%s", tempType);
+
+        int j;
+        for (j = 0; j < medCount; j++) {
+            if (strcmp(tempName, arr[j].nameMed) == 0) {
+                printf("Enter quantity: ");
+                scanf("%d", &tempQuant);
+
+                if (tempQuant > arr[j].quantity) {
+                    printf("Stock-Over\n");
+                } else {
+                    arr[j].quantity -= tempQuant;
+                    buy[buyCount] = arr[j];
+                    buy[buyCount].quantity = tempQuant;
+                    buyCount++;
+                }
+                break;
+            }
+        }
+
+        if (j == medCount) {
+            printf("Not-Found\n");
+        }
+
+        printf("Do you wish to purchase more medicine?\nAns: ");
+        scanf(" %c", &opt);
+    } while (opt == 'y');
+
+    // Print the bill
+    printBill(buy, buyCount);
+}
+
+void removeMedicine(medicine arr[], int *medCount) {
+    char opt;
+
+    do {
+        char tempName[50], tempType[20];
+
+        printf("Enter medicine name: ");
+        scanf("%s", tempName);
+        printf("Enter medicine type: ");
+        scanf("%s", tempType);
+
+        int j;
+        for (j = 0; j < *medCount; j++) {
+            if (strcmp(tempName, arr[j].nameMed) == 0) {
+                (*medCount)--;
+                for (int k = j; k < *medCount; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                break;
+            }
+        }
+
+        if (j == *medCount) {
+            printf("Not-Found\n");
+        }
+
+        printf("Do you wish to delete more medicine?\nAns: ");
+        scanf(" %c", &opt);
+    } while (opt == 'y');
+}
+
+void displayMedicineStatistics(medicine arr[], int medCount) {
+    printf("SN. ID NameOfMedicine NameOfManuf ManufDate ExpDate Quantity Price Type   TotalPrice\n");
+
+    for (int k = 0; k < medCount; k++) {
+        printf("%d. %s %s %s %s %s %d %f %s   %f\n", k + 1, arr[k].ID, arr[k].nameMed, arr[k].nameMan,
+               arr[k].manDate, arr[k].expDate, arr[k].quantity, arr[k].price, arr[k].type,
+               arr[k].quantity * arr[k].price);
+    }
+}
+
+void printBill(medicine buy[], int buyCount) {
+    printf("\nBill:\n");
+    printf("SN. ID NameOfMedicine NameOfManuf ManufDate ExpDate Quantity Price Type   TotalPrice\n");
+
+    for (int k = 0; k < buyCount; k++) {
+        printf("%d. %-10s %-15s %-15s %-10s %-10s %-8d %-8.2f %-10s %-8.2f\n", k + 1, buy[k].ID, buy[k].nameMed,
+               buy[k].nameMan, buy[k].manDate, buy[k].expDate, buy[k].quantity, buy[k].price, buy[k].type,
+               buy[k].quantity * buy[k].price);
+    }
+
+    float totalAmount = 0.0;
+    for (int k = 0; k < buyCount; k++) {
+        totalAmount += buy[k].quantity * buy[k].price;
+    }
+    printf("\nTotal amount: %.2f\n", totalAmount);
+}
